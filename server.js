a5fb195045fac4286f46cb634b1da4ce4a7c1703
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-09-18 19:52:40 -0400

    added code to server.js, added gitignore file)

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
+
 const port = 3001
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 app.use(express.static('assets'));
 
 
-app.get('./')
+app.get('./', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('api/notes', (req, res) => {
+    res.json(`${req.method} request received to get notes`);
+
+console.info(`${req.method} request received to get notes`);
+});
+
