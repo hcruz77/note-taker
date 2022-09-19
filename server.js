b96db1db19b7337c473a05a26e663f6f04a1d8e7
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-09-19 17:07:07 -0400

    worked on js to get notes to display

--- server.js
+++ server.js
@@ -1,24 +1,50 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { readAndAppend, readFromFile } = require('./utils/fsUtils');
 
 
-const port = 3001
+const PORT = 3001
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
-app.use(express.static('assets'));
+app.use(express.static('public'));
 
-
-app.get('./', (req, res) =>
+// GET Routes
+app.get('/', (req, res) =>
 res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-app.get('api/notes', (req, res) => {
-    res.json(`${req.method} request received to get notes`);
+app.get('/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
-console.info(`${req.method} request received to get notes`);
+// API Routes
+
+app.get('api/notes', (req, res) => {
+    readFromFile('db/db.json', 'utf8')
+    .then((notes) => {
+        let parsedNotes;
+
+        try {
+            parsedNotes = [].concat(JSON.parse(notes));
+        } catch (err) {
+            parsedNotes = [];
+        }
+
+        return parsedNotes;
+    })
+    .then((notes) => { return res.json(notes) })
+    .catch((err) => res.status(500).json(err))
+
 });
 
+// app.post('/notes', (req, res) => {
+//     readAndAppend(req.body, 'db/db.json')
+//     .then((note) => res.json(note))
+//     .catch((err) => res.status(500).json(err))
+// })
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
