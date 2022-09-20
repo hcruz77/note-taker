1aa75fc045a87080774c4f794903b225768949ce
Gypsy Bird <110423762+hcruz77@users.noreply.github.com>
2022-09-20 19:36:46 -0400

    created js to log notes, tested in insomnia

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const { readAndAppend, readFromFile } = require('./utils/fsUtils');
 
 
 const PORT = 3001
 const app = express();
+
+
+
+const getData = (filepath) => { 
+    const data = fs.readFileSync(filepath, 'utf-8');
+    if (!data) throw Error('no data present');
+    return JSON.parse(data);
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
@@ -23,28 +30,15 @@ res.sendFile(path.join(__dirname, '/public/notes.html'))
 
 // API Routes
 
-app.get('api/notes', (req, res) => {
-    readFromFile('db/db.json', 'utf8')
-    .then((notes) => {
-        let parsedNotes;
+app.get('/api/notes', (req, res) =>  res.json(getData('./db/db.json')));
 
-        try {
-            parsedNotes = [].concat(JSON.parse(notes));
-        } catch (err) {
-            parsedNotes = [];
-        }
-
-        return parsedNotes;
-    })
-    .then((notes) => { return res.json(notes) })
-    .catch((err) => res.status(500).json(err))
-
+app.post('/api/notes', (req, res) => {
+    const data = getData('./db/db.json');
+    data.push(req.body);
+    fs.writeFileSync('./db/db.json', JSON.stringify(data, null, 2));
+    res.json(data);
 });
 
-// app.post('/notes', (req, res) => {
-//     readAndAppend(req.body, 'db/db.json')
-//     .then((note) => res.json(note))
-//     .catch((err) => res.status(500).json(err))
-// })
+
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
